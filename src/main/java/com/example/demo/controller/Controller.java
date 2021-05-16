4959f5c93825d9fd2c398f774e47ae6edf0687ef
krishnadontaraju <69509421+krishnadontaraju@users.noreply.github.com>
2021-05-16 21:54:32 +0530

    Add files via upload

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +22,12 @@ public class Controller {
 	public String sayHello(@RequestParam(value = "name") String name) {
 		return "Hello " + name ;
 		
+	}
+	
+	//Use case 3 to pass query as path variable
+	@GetMapping("/param/{name}")
+	public String sayHelloFromParameter(@PathVariable String name) {
+		return " Hello " + name;
 	}
 	
 }
